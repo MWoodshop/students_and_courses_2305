06d2500a4237b882c9c4d092667b67e977b535d6
Mike Wood <mikewwood@icloud.com>
2023-05-24 11:32:16 -0400

    test: add return initialize values.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe Student do
       student = Student.new({ name: 'Morgan', age: 21 })
       expect(student).to be_a(Student)
     end
+
+    it 'returns correct initialize values' do
+      student = Student.new({ name: 'Morgan', age: 21 })
+      expect(student.name).to eq('Morgan')
+      expect(student.age).to eq(21)
+      expect(student.scores).to eq([])
+    end
   end
 end
