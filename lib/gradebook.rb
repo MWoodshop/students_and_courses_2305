3593c97e4372e8f1838cc0e6b394e150c0abd922
Mike Wood <mikewwood@icloud.com>
2023-05-24 12:36:34 -0400

    Feat: list_all_students method

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -21,4 +21,12 @@ class Gradebook
   def add_course(course)
     @courses << course
   end
+
+  def list_all_students
+    all_students = {}
+    @courses.each do |course|
+      all_students[course] = course.students
+    end
+    all_students
+  end
 end
