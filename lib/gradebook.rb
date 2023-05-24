b4e47bcd6ab76971cf6ae34d5ef4d6080411bb30
Mike Wood <mikewwood@icloud.com>
2023-05-24 12:08:16 -0400

    Feat: add Gradebook class and initialize.

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,9 @@
 class Gradebook
-  
+  attr_reader :instructor, :courses
+# Create a Gradebook class. Each gradebook has an instructor, as well as a way to read that data.
+
+  def initialize(instructor)
+    @instructor = instructor
+    @courses = []
+  end
 end
