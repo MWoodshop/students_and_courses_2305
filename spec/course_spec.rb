e638457cb533499bd08d59257ee192b174ef0add
Mike Wood <mikewwood@icloud.com>
2023-05-24 11:43:38 -0400

    Test: add Course class initialize test.

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -2,6 +2,11 @@ require 'rspec'
 require './lib/course'
 require './lib/student'
 
-RSpec.describe Course do 
-
+RSpec.describe Course do
+  describe 'initialize' do
+    it 'can initialize' do
+      course = Course.new('Calculus', 2)
+      expect(course).to be_a(Course)
+    end
+  end
 end
