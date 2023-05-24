04c73c928ff5f24e0782fb96b12faafc8b33b696
Mike Wood <mikewwood@icloud.com>
2023-05-24 11:34:37 -0400

    test: add can log_scores test.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -15,4 +15,13 @@ RSpec.describe Student do
       expect(student.scores).to eq([])
     end
   end
+
+  describe 'log_scores' do
+    it 'can log scores' do
+      student = Student.new({ name: 'Morgan', age: 21 })
+      student.log_scores(89)
+      student.log_scores(78)
+      expect(student.scores).to eq([89, 78])
+    end
+  end
 end
